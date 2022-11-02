1f579ff7e9f9ce8a9186fbd4f7961ae613d5200f
przekaska <kacper.blaszkowski.12@gmail.com>
2022-11-02 15:01:48 +0100

    Created game_loop and added movement

--- src/torley-run.c
+++ src/torley-run.c
@@ -1,11 +1,32 @@
 #include <ncurses.h>
+#include <unistd.h>
 
 #include "setup_ncurses.c"
+#include "player.c"
+
+
+void game_loop(){
+    char key = 0;
+    struct Player player;
+    init_player(&player);
+
+
+    while( (key = getch()) != 10){
+        player_move(&player, key);
+        draw_player(&player);
+
+        refresh();
+        usleep(33333);
+    }
+}
+
+
 
 int main(){
     init_ncurses();
 
-    
+    game_loop();    
+    mvprintw(0, 0, "END");
 
     end_ncurses();
 }
