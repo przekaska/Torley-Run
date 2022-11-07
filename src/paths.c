f13a5d551be70e78bdccecc0b7097671d738d533
przekaska <kacper.blaszkowski.12@gmail.com>
2022-11-07 21:02:00 +0100

    Modified fork_paths according to new aproach

--- src/paths.c
+++ src/paths.c
@@ -2,6 +2,7 @@
 #define TR_PATHS_C
 
 #include <stdbool.h>
+#include <string.h>
 
 #include "player.c"
 
@@ -10,8 +11,8 @@
 
 
 struct Path{
-    int topy[WINDOW_HEIGHT + STATE_LENGTH];
-    int boty[WINDOW_HEIGHT + STATE_LENGTH];
+    int topy[WINDOW_WIDTH + STATE_LENGTH];
+    int boty[WINDOW_WIDTH + STATE_LENGTH];
 
     int maxtopy;
     int maxboty;
@@ -30,24 +31,61 @@ void continue_path(struct Path *p, int iterator){
 }
 
 
-void merge_paths(struct Path *upper_p, struct Path *lower_p, int uv, int lv, int endtopy, int endboty, int iterator){
+void merge_paths(struct Path *upper_p, struct Path *lower_p, int uvelocity, int lvelocity, int endtopy, int endboty, int iterator){
     for(int i = iterator - STATE_LENGTH; i < iterator; i++){
         upper_p->topy[i] = upper_p->topy[i - 1];
         lower_p->boty[i] = lower_p->boty[i - 1];
 
-        if(upper_p->topy[i] > endtopy)
-            upper_p->topy[i] -= uv; 
-        else if(upper_p->topy[i] < endtopy)     
-            upper_p->topy[i] = endtopy;
+        if(upper_p->topy[i] > endtopy){
+            upper_p->topy[i] -= uvelocity; 
+            if(upper_p->topy[i] < endtopy)     
+                upper_p->topy[i] = endtopy;
+        }
 
-        if(lower_p->boty[i] < endboty)
-            lower_p->boty[i] += lv; 
-        else if (lower_p->boty[i] > endboty)
-            lower_p->boty[i] = endboty;
-        
+        if(lower_p->boty[i] < endboty){
+            lower_p->boty[i] += lvelocity; 
+            if (lower_p->boty[i] > endboty)
+                lower_p->boty[i] = endboty;
+        }
+
         upper_p->boty[i] = upper_p->topy[i] - rand()%(upper_p->topy[i] - endboty);
         lower_p->topy[i] = lower_p->boty[i] + rand()%(endtopy - lower_p->boty[i]);
     }
+}
+
+
+void fork_paths(struct Path *upper_p, int uvelocity, int lvelocity, int iterator,
+                 int end_up_topy, int end_up_boty, int end_lp_topy, int end_lp_boty){
+
+    static struct Path lower_p;
+    memcpy(lower_p.topy, upper_p->topy, WINDOW_WIDTH + STATE_LENGTH);    
+    memcpy(lower_p.boty, upper_p->boty, WINDOW_WIDTH + STATE_LENGTH);
+
+    for(int i = iterator - STATE_LENGTH; i < iterator; i++){
+        upper_p->boty[i] = upper_p->boty[i - 1];
+        lower_p.topy[i] = lower_p.topy[i - 1];
+
+        if(upper_p->boty[i] < end_up_boty){
+            upper_p->boty[i] += uvelocity;
+            if(upper_p->boty[i] >= end_up_boty)
+                upper_p->boty[i] = end_up_boty;
+        }
+
+        if(lower_p.topy[i] > end_lp_topy){
+            lower_p.topy[i] -= lvelocity;
+            if(lower_p.topy[i] <= end_lp_topy)
+                lower_p.topy[i] = end_lp_topy;
+        }
+
+        upper_p->topy[i] = upper_p->boty[i] + rand()%(end_up_topy - upper_p->boty[i]);
+        lower_p.boty[i] = lower_p.topy[i] - rand()%(lower_p.topy[i] - end_lp_boty);
+    }    
+
+    upper_p->maxtopy = end_up_topy;
+    upper_p->maxboty = end_up_boty;
+
+    lower_p.maxtopy = end_lp_topy;
+    lower_p.maxboty = end_lp_topy;
 }
 
 
