6322c28f01be024e63a1011215608bd4ae74a96e
przekaska <kacper.blaszkowski.12@gmail.com>
2022-11-07 19:34:50 +0100

    Changed merge_paths according to new approach

--- src/paths.c
+++ src/paths.c
@@ -30,6 +30,26 @@ void continue_path(struct Path *p, int iterator){
 }
 
 
+void merge_paths(struct Path *upper_p, struct Path *lower_p, int uv, int lv, int endtopy, int endboty, int iterator){
+    for(int i = iterator - STATE_LENGTH; i < iterator; i++){
+        upper_p->topy[i] = upper_p->topy[i - 1];
+        lower_p->boty[i] = lower_p->boty[i - 1];
+
+        if(upper_p->topy[i] > endtopy)
+            upper_p->topy[i] -= uv; 
+        else if(upper_p->topy[i] < endtopy)     
+            upper_p->topy[i] = endtopy;
+
+        if(lower_p->boty[i] < endboty)
+            lower_p->boty[i] += lv; 
+        else if (lower_p->boty[i] > endboty)
+            lower_p->boty[i] = endboty;
+        
+        upper_p->boty[i] = upper_p->topy[i] - rand()%(upper_p->topy[i] - endboty);
+        lower_p->topy[i] = lower_p->boty[i] + rand()%(endtopy - lower_p->boty[i]);
+    }
+}
+
 
 
 #endif
